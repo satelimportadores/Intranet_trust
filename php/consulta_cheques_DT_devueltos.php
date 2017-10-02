d8f15af7389e4660bb571105b7520d5c96af2208
Satel Importadores <satelimportadores@gmail.com>
2017-10-02 11:02:41 -0500

    Arreglo error JSON data table no hay registros
    Arreglo error JSON data table no hay registros

--- php/consulta_cheques_DT_devueltos.php
+++ php/consulta_cheques_DT_devueltos.php
@@ -6,9 +6,9 @@
           $sql01 = "SELECT ici.id,ici.fecha,ici.numero_cheque,ici.banco_emisor,ib.nom_banco,ici.beneficiario,ici.fecha_cheque,itc.cardname,ici.endoso,ici.estado FROM intranet_cheques_info ici INNER JOIN intranet_bancos ib ON ici.banco_emisor = ib.id INNER JOIN intranet_trust_clientes itc ON ici.beneficiario = itc.cardcode WHERE ici.estado = 'devuelto'";
           $Rcheques = $cheques->query($sql01) or trigger_error($cheques->error);
   //traer cheques 
-          
-          if (!$Rcheques) {
-             echo 'No hay datos para mostrar';
+          $num_filas =  $Rcheques->num_rows;
+          if ($num_filas == 0) {
+           echo '{ "data": [ { "0": "","1": "","2":"","3":"","4":"","5":"","6":"","7":"","8":"","9":"","id": "No hay registros...","fecha": "","numero_cheque":"","banco_emisor":"","nom_banco":"","beneficiario":"","fecha_cheque":"","cardname":"","endoso":"","estado":"" } ] }';
           }else{
             while ($data = $Rcheques->fetch_array()) {
               $arreglo['data'][] =  $data;
